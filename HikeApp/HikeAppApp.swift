e6ec066f7a41e4b26ec2d76525b1b798fb5f0d65
hackwithrama <rajram92@hotmail.com>
2024-06-28 08:32:06 +0530

    Initial Commit

---
+++ HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by Ramachandran Varadaraju on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
