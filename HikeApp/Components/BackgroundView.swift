c7cb0b0219c19507cf5d8bc04452bb252e7697ee
hackwithrama <rajram92@hotmail.com>
2024-06-28 09:13:51 +0530

    Change made to generate cardview

--- HikeApp/Components/BackgroundView.swift
+++ HikeApp/Components/BackgroundView.swift
@@ -9,10 +9,29 @@ import SwiftUI
 
 struct BackgroundView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.colorGreenDark
+                .cornerRadius(40)
+                .offset(y: 10)
+            
+            Color.colorGreenLight
+                .cornerRadius(40)
+                .opacity(0.8)
+                .offset(y: 5)
+            
+            LinearGradient(
+                colors: [Color.colorGreenLight, Color.colorGreenMedium],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .cornerRadius(40)
+            
+
+        }
     }
 }
 
 #Preview {
     BackgroundView()
+        .padding()
 }
--- HikeApp/Components/CircleView.swift
+++ HikeApp/Components/CircleView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct CircleView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Circle()
+            .fill(.indigo)
+            .frame(width: 200, height: 200)
     }
 }
 
